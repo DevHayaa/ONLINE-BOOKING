e10a198f54e77db53b867de4a50e39bd154f7d0b
Hayaa <hayaa.official10@gmail.com>
2025-04-06 16:24:07 +0500

    FIRST COMMIT

---
+++ ONLINE BOOKING/src/app/page.tsx
@@ -0,0 +1,9 @@
+import BookingForm from '@/components/BookingForm';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-r from-blue-100 to-yellow-100 flex justify-center items-center">
+      <BookingForm />
+    </main>
+  );
+}
---
+++ ONLINE BOOKING/src/components/index.ts
@@ -0,0 +1,5 @@
+"use client";
+
+export * from "./commonComponents/footer";
+export * from "./layout";
+
